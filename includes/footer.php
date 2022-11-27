516a5e5de6ba0010e1cf1803a6828a50f154d7ab
meeAsim <86559854+meeAsim@users.noreply.github.com>
2022-11-27 01:05:16 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer id="footer">
+
+  <div class="footer_bottom">
+    <p class="copyright">
+      All Copyright Reserve &copy; 2021 <a href="index.html"> <span style="font-weight:800;"> DRB Tv Network</a>
+    </p>
+
+    <p><a href="me2axim@gmail.com" style="color:white; float:right;"> Powered By :<span class="mee"> mee </span></a>
+    </p>
+  </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<section class="head">
+  <nav class="navbar  navbar-expand-lg navbar-dark fixed-top bg-info">
+    <div class="container-fluid">
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Nepali</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Hindi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Bhojpuri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">English</a>
+          </li>
+        </ul>
+      </div>
+      <a class="navbar-brand" href="#">
+        <span class="ds">DRB</span> <img src="img/logo.png" alt="" width="30" height="24" class="d-inline-block align-top">
+
+        <span class="ne">TV</span>
+      </a>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</section>
